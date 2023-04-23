e98693b036a7c9233ec3a4269344259f1bd189bd
asidapara <asidapara@gmail.com>
2023-04-23 19:06:05 -0400

    final demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,66 +1,86 @@
 import streamlit as st
 import openai
 import csv
+from math import ceil
+import io
 
-def generate_dummy_data(api_key, agent_description, prompt, num_examples):
+# Define the batch size (number of examples per batch)
+BATCH_SIZE = 20
+
+def generate_and_evaluate_batches(api_key, agent_description, prompt, num_examples, batch_size):
     openai.api_key = api_key
-    
-    examples = []
 
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=50 * num_examples,
-        n=1,
-        stop=None,
-        temperature=0.8,
-    )
+    # Calculate the number of batches required
+    num_batches = ceil(num_examples / batch_size)
+    all_examples = []
 
-    examples_raw = response.choices[0].text.strip().split("\n\n")
-    for ex in examples_raw:
-        input_text, output_text = ex.split("\n")
-        input_value = input_text.replace("Input: ", "").strip()
-        output_value = output_text.replace("Output: ", "").strip()
+    # Create a progress bar
+    progress_bar = st.progress(0)
 
-        # Generate grade for each input/output pair
-        grade_prompt = f"You are an AI language model ({agent_description}). The student translated the sentence '{input_value}' to '{output_value}'. Evaluate the translation and provide feedback."
-        grade_response = openai.Completion.create(
+    for batch_idx in range(num_batches):
+        # Calculate the start and end index for the current batch
+        start_idx = batch_idx * batch_size
+        end_idx = min(start_idx + batch_size, num_examples)
+
+        # Determine the number of examples to request in the current batch
+        current_batch_size = end_idx - start_idx
+        
+        # Generate examples for the current batch
+        response = openai.Completion.create(
             engine="text-davinci-003",
-            prompt=grade_prompt,
-            max_tokens=50,
+            prompt=prompt,
+            max_tokens=50 * current_batch_size,
             n=1,
             stop=None,
             temperature=0.8,
         )
-        grade_value = grade_response.choices[0].text.strip()
 
-        input_output_grade_tuple = (input_value, output_value, grade_value)
-        if len(input_output_grade_tuple) == 3:
-            examples.append(input_output_grade_tuple)
+        examples_raw = response.choices[0].text.strip().split("\n\n")
+        batch_examples = []
+        for ex in examples_raw:
+            if ex.count("\n") == 1:  # Ensure there is exactly one newline character
+                input_text, output_text = ex.split("\n")
+                input_value = input_text.replace("Input: ", "").strip()
+                output_value = output_text.replace("Output: ", "").strip()
 
-    return examples[:num_examples]
+                input_output_grade_tuple = (input_value, output_value)
+                if len(input_output_grade_tuple) == 2:
+                    batch_examples.append(input_output_grade_tuple)
+        
+        all_examples.extend(batch_examples)
 
-def save_to_csv(examples, output_file):
-    with open(output_file, mode="w", newline="", encoding="utf-8") as file:
-        csv_writer = csv.writer(file)
-        csv_writer.writerow(["Input", "Output", "Grade"])
-        for row in examples:
-            csv_writer.writerow(row)
+        # Update the progress bar
+        progress = (batch_idx + 1) / num_batches
+        progress_bar.progress(progress)
+
+    return all_examples
 
 def main():
-    st.title("Dummy Data Generator")
-    st.header("OpenAI Language Model")
+    st.title("Training Specialized Agents for the Edge")
+    st.header("Training Data Generator")
 
     api_key = st.text_input("Enter your OpenAI API key:")
-    agent_description = st.text_input("Enter the agent description:", value="You are a language tutor who is tasked with giving feedback on a student's translation of a german sentence. The student's grade level is 6th grade and you should follow the following principles when giving feedback: * be particularly helpful and reference parts of the translated/target sentences in quotes * be helpful, do not e.g. scold the student * please provide two parts of your feedback: 1) an overall grade Needs work, Good, Excellent; and 2) a note on what could be improved")
+    agent_description = st.text_input("Enter the agent description:", value="You are a language tutor who is tasked with giving feedback on a student's translation of a German sentence. The student's grade level is 6th grade and you should follow the following principles when giving feedback: * be particularly helpful and reference parts of the translated/target sentences in quotes * be helpful, do not e.g. scold the student * please provide two parts of your feedback: 1) an overall grade Needs work, Good, Excellent; and 2) a note on what could be improved")
     example_type = st.text_input("Enter the types of example training data to create (input, output):", value="Input: Ich bin ein Geheimagent. Output: I am a secret agent.")
     num_examples = st.number_input("Enter the number of examples you want:", min_value=1, value=10, step=1)
-    output_file = st.text_input("Enter the output CSV filename:", value="generated_dummy_data.csv")
 
     if st.button("Generate Examples"):
         prompt = f"You are an AI language model ({agent_description}). Generate {num_examples} examples of {example_type} training data. Strictly follow the Input/Output format, and only return the Input and Output."
-        generated_examples = generate_dummy_data(api_key, agent_description, prompt, num_examples)
-        save_to_csv(generated_examples, output_file)
+        generated_examples = generate_and_evaluate_batches(api_key, agent_description, prompt, num_examples, BATCH_SIZE)
+
+        csv_output = io.StringIO()
+        csv_writer = csv.writer(csv_output)
+        csv_writer.writerow(["Input", "Output"])
+        for row in generated_examples:
+            csv_writer.writerow(row)
+        csv_content = csv_output.getvalue()
+        # Add a download button for the generated CSV file
+        st.download_button(
+            label="Download CSV",
+            data=csv_content,
+            file_name="generated_dummy_data.csv",
+            mime="text/csv",
+        )
         st.success(f"Generated {len(generated_examples)} examples")
 
 if __name__ == "__main__":
